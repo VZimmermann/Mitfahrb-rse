4b207954e78c19a59093bbaaaeb684ecdbd85f59
VZimmermann <vanessa.carmen.zimmermann@hs-furtwangen.de>
2015-01-20 21:57:56 +0100

    Create nachrichten.php

---
+++ nachrichten.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>HFU Mitfahrgelegenheit- Meine Nachrichten</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta http-equiv="expires" content="0">
+	<meta http-equiv="pragma" content="no-cache">
+	<meta http-equiv="cache-control" content="no-cache">
+    <meta name="author" content="Julia Klimesch, Lisa Wojtynek,Vanessa Zimmermann" />
+	<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7" />
+	<meta name="keywords" content="HFU, Hochschule Furtwangen, Mitfahrgelegenheit, Furtwangen, Hochschule">
+	<meta name="description" content="Auf dieser Seite finden Sie die HFU Mitfahrgelegenheit. Konzipiert nur f&uuml;r Studenten und Angestellte der Hochschule Furtwangen.">
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="mitfahrgelegenheit.css" />
+
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+	
+	
+  </head>
+<body>
+<!--header-->
+<div class="container">
+	<div class="row">
+		<!-- message.php ruft die Unterseiten compose, inbox, outbox, read auf; in diesen ist der Header hinterlegt-->
+		<!--wenn hier schon der Header drin wäre würde es eine doppelte Anzeige geben-->
+		<?php include 'messages.php'?>
+	
+<!-- message.php ruft die Unterseiten compose, inbox, outbox, read auf; in diesen ist der Footer hinterlegt-->
+<!--wenn hier schon der Footer	 drin wäre würde es eine doppelte Anzeige geben-->
+</div><!-- Ende .row -->
+
+
+</div><!--Ende .container -->
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
