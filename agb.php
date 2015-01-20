0b75b8eef194297186c7bc89e4828be7af1a36d1
VZimmermann <vanessa.carmen.zimmermann@hs-furtwangen.de>
2015-01-20 21:52:50 +0100

    Create agb.php

---
+++ agb.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de" lang="de">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>HFU Mitfahrgelegenheit - Allgemeine Geschäftsbedingungen</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta http-equiv="expires" content="0">
+	<meta http-equiv="pragma" content="no-cache">
+	<meta http-equiv="cache-control" content="no-cache">
+    <meta name="author" content="Julia Klimesch, Lisa Wojtynek, Vanessa Zimmermann" />
+	<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7" />
+	<meta name="keywords" content="HFU, Hochschule Furtwangen, Mitfahrgelegenheit, Furtwangen, Hochschule">
+	<meta name="description" content="Auf dieser Seite finden Sie die HFU Mitfahrgelegenheit. Konzipiert nur f&uuml;r Studenten und Angestellte der Hochschule Furtwangen.">
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="mitfahrgelegenheit.css" />
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+	
+  </head>
+<body>
+
+<div class="container">
+
+	<!--Header-->
+	<?php include 'header.php'?>
+
+	<!--Hauptteil-->
+	<div class="row">
+		<h2>Allgemeine Geschäftsbedingungen für die Nutzung der HFU Mitfahrbörse</h1>
+		<br>
+		<p><b>Die Mitfahrb&ouml;rse mit der URL www.mitfahrboerse.hs-furtwangen.de  wird durch die Hochschule Furtwangen vertreten. </b><p>
+	
+		<p><b>&sect;1 Allgemeines</b></p>
+		<p>
+			Der Nutzer der mitfahrboerse.hs-furtwangen.de  hat die M&ouml;glichkeit sich auf der Mitfahrb&ouml;rse der HFU absolut kostenlos zu registrieren. Voraussetzung daf&uuml;r ist ein g&uuml;ltiger Account im HFU Shibboleth-System. 
+		</p>	
+		<p>
+			Der Nutzer der mitfahrboerse.hs-furtwangen.de  ist verpflichtet sein Passwort geheim zu halten und keinem Dritten den Zugang &uuml;ber seine Zugangsdaten zu gew&auml;hren.
+		</p>
+		<p>		
+			S&auml;mtliche Leistungen der Mitgliedschaft auf der Mitfahrb&ouml;rse werden f&uuml;r den Nutzer kostenlos zur Verf&uuml;gung gestellt. 
+		</p>
+		<p>		
+			Die HFU Mitfahrb&ouml;rse stellt ausschlie&szlig;lich eine Plattform f&uuml;r die Pr&auml;sentation von Mitfahrgelegenheit der Nutzer zur Verf&uuml;gung und &uuml;bernimmt ausschlie&szlig;lich die Vermittlung der Mitfahrgelegenheiten in technischer Hinsicht. mitfahrboerse.hs-furtwangen.de  pr&uuml;ft weder Angebote von Fahrern noch Nachfragen von Mitfahrern. 
+		</p>
+		<p>
+			Der Nutzer von mitfahrboerse.hs-furtwangen.de  muss selber beurteilen, ob die Fahrgemeinschaft f&uuml;r Ihn zuverl&auml;ssig und sicher genug ist. Jeder Nutzer von mitfahrboerse.hs-furtwangen.de  handelt auf eigene Gefahr.
+			mitfahrboerse.hs-furtwangen.de  &uuml;bernimmt keine Haftung oder Gew&auml;hrleistung, somit besteht kein Anspruch auf Entsch&auml;digung f&uuml;r die entstandenen Kosten oder f&uuml;r nicht stattgefundene Mitfahrgelegenheiten.
+		</p>
+		
+		<p><b>&sect;2 Nutzerpflichten</b></p>
+		<p>
+			Die Nutzung von mitfahrboerse.hs-furtwangen.de ist nur f&uuml;r private Zwecke gestattet. Jeder Hinweis auf Werbebotschaften anderer Anbieter oder auf kommerzielle Angebote ist verboten. 
+			Es ist dem Nutzer ausdr&uuml;cklich untersagt auf mitfahrboerse.hs-furtwangen.de Verlinkungen zu anderen Webseiten zu platzieren.
+		</p>
+		<p>
+			Der Nutzer ist verpflichtet die geltenden Nutzungsbedingungen zu beachten und einzuhalten.
+			Unter anderem ist es verboten auf der mitfahrboerse.hs-furtwangen.de jugendgef&auml;hrdende, gewaltverherrlichende, pornographische oder rassistische Inhalte zu ver&ouml;ffentlichen. 
+			mitfahrboerse.hs-furtwangen.de beh&auml;lt sich das Recht vor jeden auf der Mitfahrb&ouml;rse mitfahrboerse.hs-furtwangen.de Mitfahrgelegenheit oder Botschaft platzierten Eintrag zu korrigieren oder komplett zu stornieren, ohne dabei den Nutzer um Erlaubnis zu fragen oder ihn dar&uuml;ber zu informieren. 
+			mitfahrboerse.hs-furtwangen.de beh&auml;lt sich das Recht vor ein Nutzerkonto zu sperren oder komplett zu l&ouml;schen. Dabei wird die HFU-Mail des Nutzers gesperrt, um dem jeweiligen Nutzer weitere Aktivit&auml;ten auf der Mitfahrb&ouml;rse mitfahrboerse.hs-furtwangen.de zu erschweren. 
+		</p>
+		<p>
+			Der Nutzer hat hinsichtlich der von ihm ver&ouml;ffentlichten Inhalten und Materialien sicherzustellen, dass diese keine Rechte Dritter verletzen. Im Fall von Rechtenverletzung tr&auml;gt mitfahrboerse.hs-furtwangen.de keine Verantwortung und entzieht sich allen Anspr&uuml;chen auf Entsch&auml;digung. 
+		</p>
+		<p>
+			T&auml;tigkeiten von Nutzern der Mitfahrb&ouml;rse mitfahrboerse.hs-furtwangen.de, mit dem Ziel, die Mitfahrb&ouml;rse mitfahrboerse.hs-furtwangen.de funktionsuntauglich zu machen oder die Nutzung der Mitfahrb&ouml;rse mitfahrboerse.hs-furtwangen.de zu erschweren, sind verboten und werden rechtlich verfolgt. 
+		</p>
+		<p>
+			Ebenfalls verboten ist Nutzung von automatisierten Verfahren zum Speichern von Daten und Scripts, sowie der Einsatz anderer Arten von Software. 
+			Die auf der Mitfahrplattform ver&ouml;ffentlichten Daten, Anzeigen, Mitfahrgelegenheiten d&uuml;rfen nur zu privaten Zwecken verwendet werden.
+		</p>
+		<p>
+			mitfahrboerse.hs-furtwangen.de ist berechtigt, diese Allgemeinen Gesch&auml;ftsbedingungen mit Zustimmung des Kunden zu &auml;ndern, sofern die &Auml;nderungen keine Kosten f&uuml;r die Kunden beinhaltet.
+			Wir betonen ausdr&uuml;cklich, dass wir keinerlei Einfluss auf die Gestaltung und die Inhalte der gelinkten Seiten haben und distanzieren uns hiermit ausdr&uuml;cklich von allen Inhalten aller gelinkten Seiten auf der mitfahrboerse.hs-furtwangen.de.
+		</p>
+
+	</div> <!-- Ende .row -->
+
+	<!--Footer-->
+	<?php include 'footer.php'?>
+
+</div><!--Ende .container -->
+
+<!--
+der folgende Teil ist notwendig und ist Teil des Basic Templates von bootstrap 
+http://getbootstrap.com/getting-started/#template
+-->
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
