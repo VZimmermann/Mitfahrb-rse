116605dd3e1504b7d5633bac1b0e2d0e5f5b786d
VZimmermann <vanessa.carmen.zimmermann@hs-furtwangen.de>
2015-01-20 21:55:50 +0100

    Create impressum.php

---
+++ impressum.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de" lang="de">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>HFU Mitfahrgelegenheit - Impressum</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta http-equiv="expires" content="0">
+	<meta http-equiv="pragma" content="no-cache">
+	<meta http-equiv="cache-control" content="no-cache">
+    <meta name="author" content="Julia Klimesch, Lisa Wojtynek, Vanessa Zimmermann" />
+	<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7" />
+	<meta name="keywords" content="HFU, Hochschule Furtwangen, Mitfahrgelegenheit, Furtwangen, Hochschule">
+	<meta name="description" content="Auf dieser Seite finden Sie die HFU Mitfahrgelegenheit. Konzipiert nur f&uuml;r Studenten und Angestellte der Hochschule Furtwangen.">
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="mitfahrgelegenheit.css" />
+
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+<body>
+
+<div class="container">
+
+	<!--Header-->
+	<?php include 'header.php'?>
+
+	<!--Hauptteil-->
+	<div class="row">
+		<h2>Impressum</h2><br>
+		<p>Die nachstehenden Informationen enthalten die gesetzlich vorgesehenen Pflichtangaben zur Anbieterkennzeichnung 
+		und weitere Hinweise zur Internetpräsenz "hs-furtwangen.de" </p>
+		<br>
+		<p>
+			<b>Anbieter dieser Internetseiten:</b><br>
+			Hochschule Furtwangen<br>
+			Robert-Gerwig-Platz 1<br>
+			78120 Furtwangen<br>
+			Telefon 07723 920-0<br>
+			vertreten durch Prof. Dr. Stefan Noll. 
+		</p>
+		<br>
+		<p>
+			<b>Redaktion:</b><br>
+			Jutta Neumann<br>
+			E-Mail: onlineredaktion@hs-furtwangen.de<br>
+			Tel. 07723 920-2734 
+		</p>
+		<br>
+		<p>
+			<b>Haftung für Inhalte:</b><br>
+			Die Hochschule Furtwangen ist um Richtigkeit und Aktualität der auf dieser Internetpräsenz bereitgestellten Informationen bemüht.
+			Trotzdem können Fehler und Unklarheiten nicht vollständig ausgeschlossen werden. 
+			Die Hochschule Furtwangen übernimmt deshalb keine Gewähr für die Aktualität, Richtigkeit, Vollständigkeit oder Qualität der bereitgestellten Informationen. 
+		</p>
+		<br>
+		<p>
+			<b>Copyright / Urheberrecht</b><br>
+			Das Layout der Homepage, Bilder, Texte, Grafiken, Ton- und Videodokumente, Filme und andere multimediale Dateien auf den 
+			Internetseiten der Hochschule Furtwangen sind urheberrechtlich geschützt. Die Vervielfältigung, Bearbeitung, Einspeicherung,
+			Verarbeitung und Wiedergabe dieser Dateien und Inhalte bedarf der ausdrücklichen Zustimmung der Hochschule Furtwangen. 
+			Die Zustimmung ist bei der Redaktion schriftlich zu beantragen und wird in Schriftform erteilt. 
+			Alle Urheberrechte an Dateien, Texten, Bildern, Filmen, Ton- und Videoaufzeichnungen und sonstigen multimedialen Dateien,
+			ausführbaren Programmen und sonstigen Publikationen Dritter werden respektiert und sind uneingeschränkt geschützt.
+			Alle Marken- und Warenzeichen innerhalb der Homepage der Hochschule Furtwangen unterliegen uneingeschränkt dem jeweils gültigen 
+			Kennzeichnungsrecht und sind durch die Rechte Dritter geschützt. 
+		</p>
+		<br>
+		<p>
+			<b>Inhaltlich Verantwortlich:</b><br>
+			Für den Internet-Auftritt der HFU Mitfahrbörse ist die Redaktion verantwortlich. 
+			Die einzelnen Einrichtungen, Fakultäten und Institute sind für die angebotenen Inhalte, Erstellung und Pflege ihrer Webseiten zuständig. 
+		</p>
+	</div>
+
+	<!--Footer-->
+	<?php include 'footer.php'?>
+
+</div><!--Ende .container-->
+
+<!--
+der folgende Teil ist notwendig und ist Teil des Basic Templates von bootstrap 
+http://getbootstrap.com/getting-started/#template
+-->
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
