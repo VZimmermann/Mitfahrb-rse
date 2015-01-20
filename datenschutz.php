6bb6ebcab2b7108d24aff50ee79309ceaca91397
VZimmermann <vanessa.carmen.zimmermann@hs-furtwangen.de>
2015-01-20 21:53:28 +0100

    Create datenschutz.php

---
+++ datenschutz.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de" lang="de">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>HFU Mitfahrgelegenheit - Datenschutzbestimmungen</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta http-equiv="expires" content="0">
+	<meta http-equiv="pragma" content="no-cache">
+	<meta http-equiv="cache-control" content="no-cache">
+    <meta name="author" content="Julia Klimesch, Lisa Wojtynek, Vanessa Zimmermann" />
+	<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7" />
+	<meta name="keywords" content="HFU, Hochschule Furtwangen, Mitfahrgelegenheit, Furtwangen, Hochschule">
+	<meta name="description" content="Auf dieser Seite finden Sie die HFU Mitfahrgelegenheit. Konzipiert nur f&uuml;r Studenten und Angestellte der Hochschule Furtwangen.">
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="mitfahrgelegenheit.css" />
+
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+<body>
+
+
+<div class="container">
+	<!--Header-->
+	<?php include 'header.php'?>
+
+	<!--Hauptteil-->
+	<div class="row">
+		<h2>Datenschutz und Sicherheit</h2>
+		<br>
+		<p><b>Die HFU Mitfahrbörse ist Betreiber des Online Portals unter der URL http://www.mitfahrboerse.hs-furtwangen.de</b></p>
+		<br>
+		<p><b>1.Erhebung und Nutzung personenbezogener Daten</b></p>
+		<p>Der Betreiber ist berechtigt, im Rahmen der gesetzlichen Regelungen personenbezogene Daten und Nutzungsdaten des Teilnehmers zu erheben, zu verarbeiten und zu nutzen,
+		soweit sie für die Begründung,inhaltliche Ausgestaltung oder &Auml;nderung eines Vertragsverh#ltnisses über die Nutzung der Services erforderlich sind.</p>
+		<p>Personenbezogene Daten werden erhoben, sobald Sie sich eingeloggt haben, da diese bereits im Shibboleth-System der HFU gespeichert sind.
+			Wir geben Ihre personenbezogenen Daten nur weiter, soweit wir hierzu berechtigt sind, z. B. im Rahmen der Auftragsdatenverarbeitung, oder hierzu verpflichtet sind, z. B. bei der Anfrage einer Staatsanwaltschaft.
+		</p>
+		<br>
+		<p><b>2.Sicherheit</b></p>
+		<p>
+			Wir setzen technische und organisatorische Sicherheitsmaßnahmen ein, um Ihre durch uns verwalteten Daten gegen zufällige oder vorsätzliche Manipulationen, Verlust, Zerstörung oder gegen den Zugriff unberechtigter Personen zu schützen.
+		</p>
+	</div> <!-- Ende .row -->
+
+	<!--Footer-->
+	<?php include 'footer.php'?>
+
+</div><!--Ende .container -->
+
+<!--
+der folgende Teil ist notwendig und ist Teil des Basic Templates von bootstrap 
+http://getbootstrap.com/getting-started/#template
+-->
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
